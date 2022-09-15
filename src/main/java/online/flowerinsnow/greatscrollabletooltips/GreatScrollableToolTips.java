4845ce5b201f94940a7eea703136ea50ab50b1c2
冬花ice <flowerinsnow@donghua.email.cn>
2022-09-15 12:59:40 +0100

    修改版本

--- src/main/java/online/flowerinsnow/greatscrollabletooltips/GreatScrollableToolTips.java
+++ src/main/java/online/flowerinsnow/greatscrollabletooltips/GreatScrollableToolTips.java
@@ -22,7 +22,7 @@ import online.flowerinsnow.greatscrollabletooltips.listener.GuiListener;
 public class GreatScrollableToolTips {
     public static final String MODID = "g_scrollable_tt";
     public static final String NAME = "GreatScrollableToolTips";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.12.2-2.0";
     private static GSConfig config;
     public static int gsScrollY = 0;
 
